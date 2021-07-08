7dbb28b5d02d2fa087394d4c1477f28b9d2fed2f
alpercun <alper-aga97@hotmail.com>
2021-07-08 16:28:54 +0300

    Refactor to eslint

--- src/lib/components/Icon.vue
+++ src/lib/components/Icon.vue
@@ -10,12 +10,15 @@
     stroke-linecap="round"
     stroke-linejoin="round"
     class="feather feather-arrow-right"
-  ><line
-    x1="5"
-    y1="12"
-    x2="19"
-    y2="12"
-  /><polyline points="12 5 19 12 12 19" /></svg>
+  >
+    <line
+      x1="5"
+      y1="12"
+      x2="19"
+      y2="12"
+    />
+    <polyline points="12 5 19 12 12 19" />
+  </svg>
 </template>
 
 <script>
