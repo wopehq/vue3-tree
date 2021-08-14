cb5b196f1b52da059b47719d777ece9a0f15bb6b
dgknca <dgknca@gmail.com>
2021-08-14 15:20:45 +0300

    update getting-started

--- docs/src/guide/sidebar.js
+++ docs/src/guide/sidebar.js
@@ -2,12 +2,7 @@ module.exports = [
   {
     title: 'Getting Started',
     collapsable: true,
-    children: ['/guide/started'],
-  },
-  {
-    title: 'Installation',
-    collapsable: true,
-    children: ['/guide/installation'],
+    children: ['/guide/'],
   },
   {
     title: 'Props',
