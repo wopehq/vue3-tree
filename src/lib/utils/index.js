de21aa2566485edb24d29178e0f746ca1ad81b98
Aykut Kardaş <aykutkrds@gmail.com>
2021-09-06 14:55:32 +0300

    Add `updateNodes` utils

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,9 +1,11 @@
 import getNodeById from "./getNodeById";
 import setNodeById from "./setNodeById";
 import updateNodeById from "./updateNodeById";
+import updateNodes from "./updateNodes";
 
 export {
     getNodeById,
     setNodeById,
-    updateNodeById
+    updateNodeById,
+    updateNodes
 }
---
+++ src/lib/utils/updateNodes.js
@@ -0,0 +1,27 @@
+function updateNodes(nodes) {
+  return nodes.map((node) => {
+    if (node.nodes) {
+      node.nodes = updateNodes(node.nodes);
+      const everyChecked = node.nodes.every((item) => item.checked);
+      const someChecked = node.nodes.some((item) => item.checked);
+      const anyDeterminate = node.nodes.some((item) => item.indeterminate);
+
+      if (everyChecked) {
+        node.checked = true;
+        node.indeterminate = false;
+      } else if (someChecked) {        
+        node.checked = false;
+        node.indeterminate = true;
+      } else if (anyDeterminate) {      
+        node.checked = false;
+        node.indeterminate = true;
+      } else if (!everyChecked) {
+        node.checked = false;
+        node.indeterminate = false;
+      }
+    }
+    return node;
+  });
+}
+
+export default updateNodes
