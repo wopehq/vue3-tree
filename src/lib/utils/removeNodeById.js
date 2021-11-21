f706ea2dcf305cdb3d8927dc326d09ba9b885875
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-21 20:38:28 +0300

    Add removeNodeById util

---
+++ src/lib/utils/removeNodeById.js
@@ -0,0 +1,18 @@
+function removeNodeById(nodes, id) {
+  if (!Array.isArray(nodes)) return;
+
+  let newNodes = [...nodes];
+
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    if (node.id === id) {
+      newNodes = newNodes.filter(item => item.id !== id);
+    } else if (node.nodes) {
+      node.nodes = removeNodeById(node.nodes, id);
+
+      if (!node.nodes.length) delete node.nodes;
+    }
+  }
+
+  return newNodes;
+}
